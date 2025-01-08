edcce79b980f47ccea70ebd96722c4fe7091c782
dev-ansh-r <rdevanshshukla26@gmail.com>
2025-01-08 18:01:13 +0530

    deploy code

--- Code-Branches/Bhuvaneshwari/main.c
+++
@@ -1,4 +0,0 @@
-#include <stdio.>
-void main(){
-    printf("Hello My name is Bhuvaneshwari and I am an IDiot just like I always were");
-}
--- Code-Branches/Devansh/header_files/eeprom.h
+++
@@ -1,8 +0,0 @@
- #ifndef XC_HEADER_EEPROM_H
-#define	XC_HEADER_EEPROM_H
-
-unsigned char ReadEEPROM(unsigned short address);
-unsigned short ReadEEPROM16b(unsigned short Address);
-void WriteEEPROM(unsigned short address, unsigned char datas);
-void WriteEeprom16B(unsigned short Address, unsigned short datas);
-#endif	/* XC_HEADER_TEMPLATE_H */
